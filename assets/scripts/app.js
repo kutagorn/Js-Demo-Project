fe6023d753c76f840492f01033a13a4cdf0e94a3
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-02 17:45:00 +0100

    Functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,19 @@
 //import { apiKey, abc as content } from "../scripts/util";
 //import apiKey from "./util.js";
-import * as util from "./util.js";
+// import * as util from "./util.js";
 //console.error(apiKey)
 
-console.log(util.default, util.abc);
+//console.log(util.default, util.abc);
+
+function greetUser(userName, message = "Hello!"){
+    return "Hi, I am " + userName + ". " + message;
+}
+function combine(a,b,c){
+    let newValue = a * b / c;
+    return newValue   
+}
+const greeting1 = greetUser("Kutay");
+const greeting2 = greetUser("Hazal");
+
+console.log(greeting1, greeting2);
+console.log(combine(10,23,4));
