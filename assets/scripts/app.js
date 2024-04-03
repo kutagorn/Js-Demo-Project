eca93c6e4665048a3076385386ce4acb463206e0
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-03 13:19:56 +0100

    arrow functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,15 +5,39 @@
 
 //console.log(util.default, util.abc);
 
-function greetUser(userName, message = "Hello!"){
-    return "Hi, I am " + userName + ". " + message;
-}
-function combine(a,b,c){
-    let newValue = a * b / c;
-    return newValue   
-}
-const greeting1 = greetUser("Kutay");
-const greeting2 = greetUser("Hazal");
+// function greetUser(userName, message = "Hello!"){
+//     return "Hi, I am " + userName + ". " + message;
+// }
+// function combine(a,b,c){
+//     let newValue = a * b / c;
+//     return newValue   
+// }
+// const greeting1 = greetUser("Kutay");
+// const greeting2 = greetUser("Hazal");
 
-console.log(greeting1, greeting2);
-console.log(combine(10,23,4));
+// console.log(greeting1, greeting2);
+// console.log(combine(10,23,4));
+
+//arrow functions
+// export default function(userName,message){
+//     console.log('Hello!');
+//     return userName,message
+// }
+//function at on top of this and the below arrow function are the same
+/*
+If your arrow function contains no other logic but a return statement, you may omit the curly braces and the return keyword.
+
+Instead of
+
+number => { 
+  return number * 3;
+}
+you could write
+
+number => number * 3;
+ */
+export default (userName,message) /*Instead of writing function before the parameter we put an arrow after the parameter*/=>{
+    return userName,message
+}
+
+
