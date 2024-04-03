4599952d482d9d6826389ffec83e3e457fcc74a1
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-03 13:37:17 +0100

    Objects and classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -36,8 +36,33 @@ you could write
 
 number => number * 3;
  */
-export default (userName,message) /*Instead of writing function before the parameter we put an arrow after the parameter*/=>{
-    return userName,message
-}
+// export default (userName,message) /*Instead of writing function before the parameter we put an arrow after the parameter*/=>{
+//     return userName,message
+// }
 
+// const user = {
+//        name: "Max",
+//        age: 34,
+//        greet() {
+//          console.log("Hello!");
+//          console.log(this.age);
+//        }
+//      };
+//      console.log(user.name);
+//      user.greet();
+     
+// class User {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     greet() {
+//         console.log(this.name);
+//         console.log("Hi!");
+//     }
+// }
+// const user1 = new User("Manuel", 35);
+// user1.greet();
 
+// console.log(user1);
+// user1.greet();
