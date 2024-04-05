84d60e586e05a6d9ada96aba49669b7f68a4ebd5
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-05 13:36:42 +0100

    statements, prompt for input and for loops

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -96,3 +96,19 @@ number => number * 3;
 //   ...user
 // };
 // console.log(extendedUser);
+
+//statements, prompt for input and for loops
+// const password = prompt("Your password");
+// if (password === "Hello") {
+//   console.log("Hello works");
+// } else if (password === "hello") {
+//   console.log("hello works");
+// } else {
+//   console.log("Access not granted.");
+// }
+
+// const hobbies = ["Sports", "Cooking"];
+
+// for (const hobby of hobbies) {
+//   console.log(hobby);
+// }
